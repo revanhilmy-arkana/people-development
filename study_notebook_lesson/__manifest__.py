ec24153c8ce0eeb0e6d8a5276434e1fd3e6ca83f
revanhilmy <revanhaidarhilmy@gmail.com>
2026-05-10 18:53:45 +0700

    [ADD] study_notebook

---
+++ study_notebook_lesson/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Study Notebook Lesson',
+    'version': '1.0',
+    'summary': 'Module for studying Notebooks and Pages (Tabs) in Form views',
+    'category': 'Education',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/menu.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
