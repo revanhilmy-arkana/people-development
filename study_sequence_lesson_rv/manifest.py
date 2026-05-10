e58dede6e20a6d9e868764ef3f1e294d6ce3c02d
revanhilmy <revanhaidarhilmy@gmail.com>
2026-05-10 09:25:45 +0700

    [ADD] study_sequence_rv

---
+++ study_sequence_lesson_rv/manifest.py
@@ -0,0 +1,18 @@
+{
+    "name": "",
+    "version": "19.0.1.0.0",
+    "category": "Purchases",
+    "summary": "",
+    "author": "",
+    "license": "LGPL-3",
+    "depends": [
+        "base",
+        "purchase",
+    ],    
+    "data": [
+        "data/ir_sequence_data.xml",
+        "views/purchase_order_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
