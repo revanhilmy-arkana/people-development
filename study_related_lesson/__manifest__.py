7dd6952aacf9540ba6817b2a67ed64c12bfba43a
revanhilmy <revanhaidarhilmy@gmail.com>
2026-05-10 19:00:46 +0700

    [ADD] study_related

---
+++ study_related_lesson/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Study Related Field Lesson',
+    'version': '1.0',
+    'summary': 'Module for studying Related Fields and automatic data mirroring',
+    'category': 'Education',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/menu.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
