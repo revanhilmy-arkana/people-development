f795813f25674500561b76a7db6af47f7491b7b7
revanhilmy <revanhaidarhilmy@gmail.com>
2026-05-10 18:40:10 +0700

    [ADD] study_security

---
+++ study_security_lesson/models/lesson.py
@@ -0,0 +1,9 @@
+from odoo import fields, models, api
+
+class SecurityLesson(models.Model):
+    _name = 'study.security.lesson'
+    _description = 'Security Lesson'
+
+    name = fields.Char(string='Title', required=True)
+    user_id = fields.Many2one('res.users', string='Responsible User', default=lambda self: self.env.user)
+    description = fields.Text(string='Private Notes')
