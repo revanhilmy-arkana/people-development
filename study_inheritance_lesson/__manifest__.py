48c27c0909ee22dfa172804c082a125b03c8672a
revanhilmy <revanhaidarhilmy@gmail.com>
2026-05-10 19:04:26 +0700

    [ADD} study_inheritance

---
+++ study_inheritance_lesson/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Study Inheritance Lesson',
+    'version': '1.0',
+    'summary': 'Module for studying Model and View Inheritance (Customizing standard Odoo)',
+    'category': 'Education',
+    'depends': ['base'],
+    'data': [
+        'views/partner_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
