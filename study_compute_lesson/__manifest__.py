660d05acf2de3e301f49f02c6d286770c1a8c562
revanhilmy <revanhaidarhilmy@gmail.com>
2026-05-12 17:24:16 +0700

    [ADD] study_compute

---
+++ study_compute_lesson/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Study Compute Field Lesson',
+    'version': '1.0',
+    'summary': 'Module for studying Compute Fields, @api.depends, and Stored calculations',
+    'category': 'Education',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/menu.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
