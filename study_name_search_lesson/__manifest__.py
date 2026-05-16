8cc650882afc67458d4f13776ffd83f731e82923
revanhilmy <revanhaidarhilmy@gmail.com>
2026-05-17 00:19:18 +0700

    study_name_search

---
+++ study_name_search_lesson/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Study Name Search Lesson',
+    'version': '1.0',
+    'summary': 'Module for studying _rec_names_search for multi-field dropdown searching',
+    'category': 'Education',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/menu.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
